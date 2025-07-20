24f9baac642a80e85a8d821f431b980993acb09f
TanishaD-test <dilawaritanisha26@gmail.com>
2025-07-20 18:22:44 +0530

    Update string_calculator_spec.rb
    - handles multiple custom delimiters with multiple length with brackets
    - handles single delimiter of longer than one char with brackets
    - ignores numbers greater than 1000

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,4 +1,4 @@
-require_relative './string_calculator'
+require_relative '../string_calculator'
 
 RSpec.describe StringCalculator do
   let(:calculator) { StringCalculator.new }
@@ -23,6 +23,26 @@ RSpec.describe StringCalculator do
     expect(calculator.add("1\n2,3")).to eq(6)
   end
 
+  it "supports custom single-character delimiter" do
+    expect(calculator.add("//;\n1;2")).to eq(3)
+  end
+
+  it "supports single custom delimiter longer than one char with brackets" do
+    expect(calculator.add("//[***]\n1***2***3")).to eq(6)
+  end
+
+  it "supports single delimiter of any length without brackets" do
+    expect(calculator.add("//***\n1***2***3")).to eq(6)
+  end
+
+  it "supports multiple delimiters" do
+    expect(calculator.add("//[*][%]\n1*2%3")).to eq(6)
+  end
+
+  it "supports multiple delimiters longer than one character" do
+    expect(calculator.add("//[**][%%]\n1**2%%3")).to eq(6)
+  end
+
   it "raises error on single negative number" do
     expect { calculator.add("1,-2,3") }
       .to raise_error(ArgumentError, "negative numbers not allowed -2")
@@ -33,11 +53,8 @@ RSpec.describe StringCalculator do
       .to raise_error(ArgumentError, "negative numbers not allowed -2,-5")
   end
 
-  it "supports custom single-character delimiter" do
-    expect(calculator.add("//;\n1;2")).to eq(3)
-  end
-
-  it "supports single delimiter of any length without brackets" do
-    expect(calculator.add("//***\n1***2***3")).to eq(6)
+  it "ignores numbers greater than 1000" do
+    expect(calculator.add("2,1001")).to eq(2)
+    expect(calculator.add("1000,1001,5")).to eq(1005)
   end
 end
