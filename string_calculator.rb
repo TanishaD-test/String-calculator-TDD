7cc6621a3c9b673ff3b664d3a898e665a4760d8f
TanishaD-test <dilawaritanisha26@gmail.com>
2025-07-20 18:16:44 +0530

    Update string_calculator.rb
    - Supports changing delimiters with //... syntax
    - Supports Single delimiter without brackets
    - supports single delimiter of any length without brackets

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,7 +1,12 @@
 class StringCalculator
   def add(numbers)
     return 0 if numbers.nil? || numbers.empty?
-    delimiters = /,|\n/
+    if numbers.start_with?("//")
+      delimiter_part, numbers = numbers[2..-1].split("\n", 2)
+      delimiters = Regexp.new(Regexp.escape(delimiter_part))
+    else
+      delimiters = /,|\n/
+    end
     nums = numbers.split(delimiters).map(&:to_i)
     negatives = nums.select(&:negative?)
     unless negatives.empty?
