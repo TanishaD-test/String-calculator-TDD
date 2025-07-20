3b61d51038d6ba78591d732d3e694ccc3afe2c73
TanishaD-test <dilawaritanisha26@gmail.com>
2025-07-20 18:21:11 +0530

    Update string_calculator.rb
    - handles multiple delimiters with brackets
    - supports multiple delimiters longer than one character
    - supports single custom delimiter longer than one char with brackets

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,18 +1,28 @@
 class StringCalculator
   def add(numbers)
     return 0 if numbers.nil? || numbers.empty?
+
     if numbers.start_with?("//")
       delimiter_part, numbers = numbers[2..-1].split("\n", 2)
-      delimiters = Regexp.new(Regexp.escape(delimiter_part))
+
+      if delimiter_part.start_with?("[") && delimiter_part.end_with?("]")
+        delimiters = delimiter_part.scan(/\[(.+?)\]/).flatten
+        regex_pattern = delimiters.map { |d| Regexp.escape(d) }.join('|')
+        delimiters = Regexp.new(regex_pattern)
+      else
+        delimiters = Regexp.new(Regexp.escape(delimiter_part))
+      end
     else
       delimiters = /,|\n/
     end
+
     nums = numbers.split(delimiters).map(&:to_i)
+
     negatives = nums.select(&:negative?)
     unless negatives.empty?
       raise ArgumentError, "negative numbers not allowed #{negatives.join(',')}"
     end
-    nums.sum
+
+    nums.reject { |n| n > 1000 }.sum
   end
 end
-      
